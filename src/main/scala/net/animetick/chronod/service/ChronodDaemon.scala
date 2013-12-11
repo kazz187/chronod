bce8ec0c3a7e63c9bbdf0acb0a7850bc06087ed9
kazz187 <kazzone87@gmail.com>
2013-12-12 03:55:42 +0900

    Add main class for startup.

---
+++ src/main/scala/net/animetick/chronod/service/ChronodDaemon.scala
@@ -0,0 +1,52 @@
+package net.animetick.chronod.service
+
+import java.io.File
+import com.typesafe.scalalogging.slf4j.Logging
+
+/**
+ * ChronodDaemon
+ */
+class ChronodDaemon extends Logging {
+
+  def setup() {
+    val javaVersion = System.getProperty("java.version")
+    val javaVmName = System.getProperty("java.vm.name")
+    logger.info("JVM vendor/version: {}/{}", javaVmName, javaVersion)
+    logger.info("Heap size: {}/{}",
+      Runtime.getRuntime.totalMemory().toString,
+      Runtime.getRuntime.maxMemory().toString)
+    logger.info("Classpath: {}", System.getProperty("java.class.path"))
+  }
+
+  def start() {
+
+  }
+
+  def stop() {
+
+  }
+
+  def activate() {
+    val pidFile = System.getProperty("chronod-pidfile")
+    setup()
+    if (pidFile != null) {
+      new File(pidFile).deleteOnExit()
+    }
+    if (System.getProperty("chronod-foreground") == null) {
+      System.out.close()
+      System.err.close()
+    }
+    start()
+  }
+
+}
+
+
+object ChronodDaemon {
+
+  def main(args: Array[String]) {
+    val daemon = new ChronodDaemon
+    daemon.activate()
+  }
+
+}
